52845cbff51d69d5284c65a0b5fe4b13d6f50f01
la-serene <ky.nguyencao23@gmail.com>
2025-01-12 20:21:25 +0700

    add client messages

---
+++ _client/messages.h
@@ -0,0 +1,9 @@
+#ifndef MESSAGES_H
+#define MESSAGES_H
+
+inline auto SEND_MOVE_UP = "MOVE UP\n";
+inline auto SEND_MOVE_DOWN = "MOVE DOWN\n";
+inline auto SEND_MOVE_LEFT = "MOVE LEFT\n";
+inline auto SEND_MOVE_RIGHT = "MOVE RIGHT\n";
+
+#endif //MESSAGES_H
