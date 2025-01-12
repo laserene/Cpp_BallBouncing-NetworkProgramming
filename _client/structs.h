6e98dfdd47c7bacd34a9096ab1d97575d5ea7b51
la-serene <ky.nguyencao23@gmail.com>
2025-01-13 04:58:17 +0700

    additional structs

--- _client/structs.h
+++ _client/structs.h
@@ -4,6 +4,26 @@
 #include "SDL2/SDL.h"
 #include "defs.h"
 
+struct Explosion {
+    float x;
+    float y;
+    float dx;
+    float dy;
+    int r, g, b, a;
+    Explosion *next;
+};
+
+struct Debris {
+    float x;
+    float y;
+    float dx;
+    float dy;
+    SDL_Rect rect;
+    SDL_Texture *texture;
+    int life;
+    Debris *next;
+};
+
 typedef struct {
     // Passsing sock and read_fds to handle server messages
     void (*logic)(int sock, const fd_set &read_fds);
@@ -35,6 +55,14 @@ struct Entity {
 typedef struct {
     Entity fighterHead, *fighterTail;
     Entity bulletHead, *bulletTail;
+    Explosion explosionHead, *explosionTail;
+    Debris debrisHead, *debrisTail;
 } Stage;
+
+typedef struct {
+    int x;
+    int y;
+    int speed;
+} Star;
 
 #endif //STRUCTS_H
